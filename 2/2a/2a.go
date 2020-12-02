ac84ba8632be99e72bb63b6e7b3fa27bf93e8c03
TauSigma5 <tausigma5@tauflight.com>
2020-12-02 13:55:10 -0800

    Finished Day 2

---
+++ 2/2a/2a.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func readLines(path string) []string {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Read Error")
+		return nil
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if err == nil {
+			lines = append(lines, scanner.Text())
+		}
+	}
+	return lines
+}
+
+func main() {
+	passwords := readLines("./passwords.txt")
+	validPasswords := 0
+
+	for i := 0; i < len(passwords); i++ {
+		fields := strings.Fields(passwords[i])
+
+		// 0th index is min, 1st index
+		minMax := strings.Split(fields[0], "-")
+
+		min, err := strconv.Atoi(minMax[0])
+
+		max, err1 := strconv.Atoi(minMax[1])
+
+		if err != nil && err1 != nil {
+			fmt.Println("Error")
+			break
+		}
+
+		// Character
+		character := strings.Trim(fields[1], ":")
+
+		// Actual Password Processing
+		appearances := strings.Count(fields[2], character)
+
+		// fmt.Println(min, max, character, appearances)
+
+		if appearances >= min && appearances <= max {
+			validPasswords++
+		}
+	}
+
+	fmt.Println(validPasswords)
+}
---
+++ 2/2b/2b.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func readLines(path string) []string {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Read Error")
+		return nil
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if err == nil {
+			lines = append(lines, scanner.Text())
+		}
+	}
+	return lines
+}
+
+func main() {
+	passwords := readLines("./passwords.txt")
+	validPasswords := 0
+
+	for i := 0; i < len(passwords); i++ {
+		fields := strings.Fields(passwords[i])
+
+		// 0th index is min, 1st index
+		pos := strings.Split(fields[0], "-")
+
+		pos1, err := strconv.Atoi(pos[0])
+		pos2, err1 := strconv.Atoi(pos[1])
+		// Position indexes from corporate don't from one
+		pos1--
+		pos2--
+
+		// Shitty code that should never see the light of day
+		if err != nil && err1 != nil {
+			fmt.Println("Error")
+			break
+		}
+
+		// Character
+		character := strings.Trim(fields[1], ":")
+
+		// Actual Password Processing
+		password := string(fields[2])
+
+		// There's no XOR function :/
+		if (string(password[pos1]) == character || string(password[pos2]) == character) && !(string(password[pos1]) == character && string(password[pos2]) == character) {
+			validPasswords++
+		}
+	}
+
+	fmt.Println(validPasswords)
+}
